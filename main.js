d07365e10dbbc2a273d78d66f8d2dec3563bf16b
Thahashaik <83202330+Thahashaik@users.noreply.github.com>
2023-07-13 23:14:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+const navOpen = () => {
+   navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+  document.body.style= 'visibility: visible; height: 100vh; width:100vw; overflow:hidden;';
+}
+
+const navClose = () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+  document.body.style= 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose)
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
